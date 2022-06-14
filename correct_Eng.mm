a64f83705df8e16d728325d838390f4aacdf1c8a
723683592 <starklee912@gmail.com>
2022-06-14 20:24:49 +0800

    英语改错题

---
+++ correct_Eng.mm
@@ -0,0 +1,106 @@
+
+<map>
+  <node ID="root" TEXT="英语语法改错-for(刘淑芝）">
+    <node TEXT="缺少句子成分" ID="uuAL8I0r8r" _mubu_text="%3Cspan%3E%E7%BC%BA%E5%B0%91%E5%8F%A5%E5%AD%90%E6%88%90%E5%88%86%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="第二个句子缺少主语" ID="SbLcWdDudM" _mubu_text="%3Cspan%3E%E7%AC%AC%E4%BA%8C%E4%B8%AA%E5%8F%A5%E5%AD%90%E7%BC%BA%E5%B0%91%E4%B8%BB%E8%AF%AD%3C/span%3E" STYLE="fork"/>
+      <node TEXT="第二个句子缺少谓语" ID="6FCwX2TgRA" _mubu_text="%3Cspan%3E%E7%AC%AC%E4%BA%8C%E4%B8%AA%E5%8F%A5%E5%AD%90%E7%BC%BA%E5%B0%91%E8%B0%93%E8%AF%AD%3C/span%3E" STYLE="fork"/>
+      <node TEXT="缺少主语和谓语" ID="woMsmI633E" _mubu_text="%3Cspan%3E%E7%BC%BA%E5%B0%91%E4%B8%BB%E8%AF%AD%E5%92%8C%E8%B0%93%E8%AF%AD%3C/span%3E" STYLE="fork"/>
+      <node TEXT="缺少主句" ID="An5cSibAun" _mubu_text="%3Cspan%3E%E7%BC%BA%E5%B0%91%E4%B8%BB%E5%8F%A5%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="逗号问题" ID="cVHEvKZSXY" _mubu_text="%3Cspan%3E%E9%80%97%E5%8F%B7%E9%97%AE%E9%A2%98%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="   逗号拼接两个完整的句子是不合适的，这时候往往需要把逗号变成句号，或者变成状语做修饰" ID="SYyplVsQ5f" _mubu_text="%3Cspan%3E%20%20%20%E9%80%97%E5%8F%B7%E6%8B%BC%E6%8E%A5%E4%B8%A4%E4%B8%AA%E5%AE%8C%E6%95%B4%E7%9A%84%E5%8F%A5%E5%AD%90%E6%98%AF%E4%B8%8D%E5%90%88%E9%80%82%E7%9A%84%EF%BC%8C%E8%BF%99%E6%97%B6%E5%80%99%E5%BE%80%E5%BE%80%E9%9C%80%E8%A6%81%E6%8A%8A%E9%80%97%E5%8F%B7%E5%8F%98%E6%88%90%E5%8F%A5%E5%8F%B7%EF%BC%8C%E6%88%96%E8%80%85%E5%8F%98%E6%88%90%E7%8A%B6%E8%AF%AD%E5%81%9A%E4%BF%AE%E9%A5%B0%3C/span%3E" STYLE="fork"/>
+      <node TEXT="  ，连接两个主句可以用连词" ID="3A22hYEfs8" _mubu_text="%3Cspan%3E%20%20%EF%BC%8C%E8%BF%9E%E6%8E%A5%E4%B8%A4%E4%B8%AA%E4%B8%BB%E5%8F%A5%E5%8F%AF%E4%BB%A5%E7%94%A8%E8%BF%9E%E8%AF%8D%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="两个主句之间没有标点符号" ID="hxTJlDRgaX" _mubu_text="%3Cspan%3E%E4%B8%A4%E4%B8%AA%E4%B8%BB%E5%8F%A5%E4%B9%8B%E9%97%B4%E6%B2%A1%E6%9C%89%E6%A0%87%E7%82%B9%E7%AC%A6%E5%8F%B7%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="Sr1Lk8jgaV" _mubu_text="" STYLE="fork"/>
+      <node TEXT="直接分成两个句子" ID="QberpCxuqz" _mubu_text="%3Cspan%3E%E7%9B%B4%E6%8E%A5%E5%88%86%E6%88%90%E4%B8%A4%E4%B8%AA%E5%8F%A5%E5%AD%90%3C/span%3E" STYLE="fork"/>
+      <node TEXT="用连词so连接" ID="etofjAFsd3" _mubu_text="%3Cspan%3E%E7%94%A8%E8%BF%9E%E8%AF%8Dso%E8%BF%9E%E6%8E%A5%3C/span%3E" STYLE="fork"/>
+      <node TEXT="分号" ID="YIGQ8GM7Xs" _mubu_text="%3Cspan%3E%E5%88%86%E5%8F%B7%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="比较的东西不是同一级别的东西" ID="8WbZ7FkWMX" _mubu_text="%3Cspan%3E%E6%AF%94%E8%BE%83%E7%9A%84%E4%B8%9C%E8%A5%BF%E4%B8%8D%E6%98%AF%E5%90%8C%E4%B8%80%E7%BA%A7%E5%88%AB%E7%9A%84%E4%B8%9C%E8%A5%BF%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="奴隶的食物不能跟狗比较，要用狗的食物" ID="vmG8UASgdK" _mubu_text="%3Cspan%3E%E5%A5%B4%E9%9A%B6%E7%9A%84%E9%A3%9F%E7%89%A9%E4%B8%8D%E8%83%BD%E8%B7%9F%E7%8B%97%E6%AF%94%E8%BE%83%EF%BC%8C%E8%A6%81%E7%94%A8%E7%8B%97%E7%9A%84%E9%A3%9F%E7%89%A9%3C/span%3E" STYLE="fork"/>
+      <node TEXT="像的不是大学而是项目" ID="B7SSki800s" _mubu_text="%3Cspan%3E%E5%83%8F%E7%9A%84%E4%B8%8D%E6%98%AF%E5%A4%A7%E5%AD%A6%E8%80%8C%E6%98%AF%E9%A1%B9%E7%9B%AE%3C/span%3E" STYLE="fork"/>
+      <node TEXT="平衡 歧义" ID="LNwlzQATUL" _mubu_text="%3Cspan%3E%E5%B9%B3%E8%A1%A1%20%E6%AD%A7%E4%B9%89%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="other else" ID="uejhYVnGgj" _mubu_text="%3Cspan%3Eother%20else%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="any other" ID="v3WZaxpJS1" _mubu_text="%3Cspan%3Eany%20other%3C/span%3E" STYLE="fork"/>
+      <node TEXT="anyone else" ID="D9sLfIYvmT" _mubu_text="%3Cspan%3Eanyone%20else%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="双重比较" ID="LC8mk4thoZ" _mubu_text="%3Cspan%3E%E5%8F%8C%E9%87%8D%E6%AF%94%E8%BE%83%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="stXE1WQmfE" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="than / to" ID="Ize9bSVidt" _mubu_text="%3Cspan%3Ethan%20/%20to%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="Asav9bdowg" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="修饰错位" ID="SUjZNLbvKk" _mubu_text="%3Cspan%3E%E4%BF%AE%E9%A5%B0%E9%94%99%E4%BD%8D%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="HZgiFLhJfT" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="KP0Lg8lNgd" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="6SDiY7pYKr" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="OwgSlgb8RH" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="oTWk077W4M" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="像副词的形容词" ID="acSPgz52qO" _mubu_text="%3Cspan%3E%E5%83%8F%E5%89%AF%E8%AF%8D%E7%9A%84%E5%BD%A2%E5%AE%B9%E8%AF%8D%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="GG08hnnjKC" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="Jeiok4n2f6" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="iJC4vPveFs" _mubu_text="" STYLE="fork"/>
+      <node TEXT="timely friendly leisurely都是形容词" ID="u44QOdLR1p" _mubu_text="%3Cspan%3Etimely%20friendly%20leisurely%E9%83%BD%E6%98%AF%E5%BD%A2%E5%AE%B9%E8%AF%8D%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="单复数问题" ID="ejdbgDXwl3" _mubu_text="%3Cspan%3E%E5%8D%95%E5%A4%8D%E6%95%B0%E9%97%AE%E9%A2%98%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="0d4xjs7qdR" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="sCejB8UDCh" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="rHqQ0ftq8A" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="fJph5Ct5dN" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="now/then并不能连接两个句子" ID="OzYGuonE09" _mubu_text="%3Cspan%3Enow/then%E5%B9%B6%E4%B8%8D%E8%83%BD%E8%BF%9E%E6%8E%A5%E4%B8%A4%E4%B8%AA%E5%8F%A5%E5%AD%90%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="必要的时候要使用and连接两个句子" ID="U0KDwBvXH0" _mubu_text="%3Cspan%3E%E5%BF%85%E8%A6%81%E7%9A%84%E6%97%B6%E5%80%99%E8%A6%81%E4%BD%BF%E7%94%A8and%E8%BF%9E%E6%8E%A5%E4%B8%A4%E4%B8%AA%E5%8F%A5%E5%AD%90%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="人称/代词对应" ID="AX6GeY1RVI" _mubu_text="%3Cspan%3E%E4%BA%BA%E7%A7%B0/%E4%BB%A3%E8%AF%8D%E5%AF%B9%E5%BA%94%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="XIMnkocFHO" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="SgrPimZrrC" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="CC9IcAyLxJ" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="6vVfVcs6aM" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="切换" ID="8yPzmbQ4I6" _mubu_text="%3Cspan%3E%E5%88%87%E6%8D%A2%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="主语切换" ID="RgetX8bG5I" _mubu_text="%3Cspan%3E%E4%B8%BB%E8%AF%AD%E5%88%87%E6%8D%A2%3C/span%3E" STYLE="fork">
+        <node TEXT="" ID="oYl0F8cfqn" _mubu_text="" STYLE="fork"/>
+        <node TEXT="" ID="2bWlmLMH2B" _mubu_text="" STYLE="fork"/>
+      </node>
+      <node TEXT="时态切换" ID="bh344wi8uz" _mubu_text="%3Cspan%3E%E6%97%B6%E6%80%81%E5%88%87%E6%8D%A2%3C/span%3E" STYLE="fork">
+        <node TEXT="" ID="Ee4xd198oP" _mubu_text="" STYLE="fork"/>
+      </node>
+      <node TEXT="感情切换" ID="8kx73c7jdd" _mubu_text="%3Cspan%3E%E6%84%9F%E6%83%85%E5%88%87%E6%8D%A2%3C/span%3E" STYLE="fork">
+        <node TEXT="" ID="4AUEmALr7m" _mubu_text="" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="语态" ID="wx978eFlPM" _mubu_text="%3Cspan%3E%E8%AF%AD%E6%80%81%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="Wp1SeyMzMT" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="IBWWJoOY0j" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="sYjOQJVhLC" _mubu_text="" STYLE="fork"/>
+      <node TEXT="到底是谁在走在林子里，什么东西用大字写的，谁要洗手" ID="5y59h8m3U5" _mubu_text="%3Cspan%3E%E5%88%B0%E5%BA%95%E6%98%AF%E8%B0%81%E5%9C%A8%E8%B5%B0%E5%9C%A8%E6%9E%97%E5%AD%90%E9%87%8C%EF%BC%8C%E4%BB%80%E4%B9%88%E4%B8%9C%E8%A5%BF%E7%94%A8%E5%A4%A7%E5%AD%97%E5%86%99%E7%9A%84%EF%BC%8C%E8%B0%81%E8%A6%81%E6%B4%97%E6%89%8B%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="当分词、不定式或它们的短语对句子的意义不是必要的时，我们将它与带有逗号的句子的其余部分分开。" ID="8V3SlsEHR8" _mubu_text="%3Cspan%3E%E5%BD%93%E5%88%86%E8%AF%8D%E3%80%81%E4%B8%8D%E5%AE%9A%E5%BC%8F%E6%88%96%E5%AE%83%E4%BB%AC%E7%9A%84%E7%9F%AD%E8%AF%AD%E5%AF%B9%E5%8F%A5%E5%AD%90%E7%9A%84%E6%84%8F%E4%B9%89%E4%B8%8D%E6%98%AF%E5%BF%85%E8%A6%81%E7%9A%84%E6%97%B6%EF%BC%8C%E6%88%91%E4%BB%AC%E5%B0%86%E5%AE%83%E4%B8%8E%E5%B8%A6%E6%9C%89%E9%80%97%E5%8F%B7%E7%9A%84%E5%8F%A5%E5%AD%90%E7%9A%84%E5%85%B6%E4%BD%99%E9%83%A8%E5%88%86%E5%88%86%E5%BC%80%E3%80%82%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="AGjybKebck" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="Wxw4rreW9I" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="68omqR7Hvz" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="不可数名词/专有名词" ID="xJo7FQn5tq" _mubu_text="%3Cspan%3E%E4%B8%8D%E5%8F%AF%E6%95%B0%E5%90%8D%E8%AF%8D/%E4%B8%93%E6%9C%89%E5%90%8D%E8%AF%8D%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="mN8ZaTJbll" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="9IHD5TSlHk" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="iu4pxuO4mb" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="XJtJR7Pws8" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="if/whether/because" ID="0QhUKNvB30" _mubu_text="%3Cspan%3Eif/whether/because%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="HTDamlhGvq" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="XkYYKyHxDr" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="c8CbdgKrpX" _mubu_text="" STYLE="fork"/>
+    </node>
+    <node TEXT="其他" ID="q6rPdEmMfV" _mubu_text="%3Cspan%3E%E5%85%B6%E4%BB%96%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="" ID="ADs7VQyR17" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="Vx1zFt6wKR" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="rbYOZ17oEC" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="JmAwI0qfH0" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="DBimnKaYmB" _mubu_text="" STYLE="fork"/>
+      <node TEXT="" ID="d71JP0NENR" _mubu_text="" STYLE="fork"/>
+    </node>
+  </node>
+</map>
